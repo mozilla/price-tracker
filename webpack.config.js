7dafaa8ab98edc53517ec3a59f10c48b3d9a0379
Bianca Danforth <biancadanforth@gmail.com>
2018-07-06 18:30:08 -0700

    Add React to sidebar in web extension.
    Uses webpack to transpile sidebar.jsx into JavaScript to render the Sidebar component, which is currently a pure React component (i.e. stateless).
    
    In addition to adding React and ReactDOM deps, I added the following devDeps:
    * babel-core: Library to do the transpilation
    * babel-loader: Enable transpiling using Babel via Webpack
    * babel-preset-env: Compiles ES2015+ down to ES5
    * babel-preset-react: Compiles JSX to JS

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const BUILD_DIR = path.resolve(__dirname, 'build');
+const SOURCE_DIR = path.resolve(__dirname, 'src');
 
 // FIXME(osmose): This file is optimized for development. At some point we
 // should add optimizations for production-ready code bundling.
@@ -12,10 +13,22 @@ module.exports = {
   target: 'web',
   entry: {
     background: './src/background.js',
+    sidebar: './src/sidebar.jsx',
   },
   output: {
     path: BUILD_DIR,
     filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        include: SOURCE_DIR,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
   },
   plugins: [
     new CopyWebpackPlugin([
