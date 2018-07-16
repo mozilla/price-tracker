ed7b8656171016a82b77ef6ead7e6f07ed99655d
Michael Kelly <mkelly@mozilla.com>
2018-07-16 12:56:53 -0700

    #23: Add Accordion component for displaying multiple sections of content.

--- src/sidebar/index.jsx
+++ src/sidebar/index.jsx
@@ -6,30 +6,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import pt from 'prop-types';
 
+import Accordion from 'commerce/sidebar/components/Accordion';
+
 const PRODUCT_KEYS = ['title', 'image', 'price'];
 
 class Sidebar extends React.Component {
+  static propTypes = {
+    product: pt.shape({
+      title: pt.string,
+      image: pt.string,
+      price: pt.string,
+    }),
+  }
+
+  static defaultProps = {
+    product: undefined,
+  }
+
+  componentDidCatch() {
+    // TODO: Show friendly message when errors happen.
+  }
+
   render() {
     const {product} = this.props;
     return (
-      <ul>
-        <li>{product.title || 'Unknown'}</li>
-        <li>{product.image || 'Unknown'}</li>
-        <li>{product.price || 'Unknown'}</li>
-      </ul>
+      <Accordion className="sidebar-container">
+        {product && (
+          <Accordion.Section title={`Current Page: ${product.title}`}>
+            <ul>
+              <li>{product.title || 'Unknown'}</li>
+              <li>{product.image || 'Unknown'}</li>
+              <li>{product.price || 'Unknown'}</li>
+            </ul>
+          </Accordion.Section>
+        )}
+        <Accordion.Section title="Collections" initial>
+          <div>Collections</div>
+        </Accordion.Section>
+      </Accordion>
     );
   }
 }
-Sidebar.propTypes = {
-  product: pt.shape({
-    title: pt.string,
-    image: pt.string,
-    price: pt.string,
-  }),
-};
-Sidebar.defaultProps = {
-  product: {},
-};
 
 // Pull in product data from query parameters
 const url = new URL(window.location);
