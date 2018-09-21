mozilla/price-tracker

commit 34f860d1554bf63e1fc3b55396b798420c85a50a

Mild background script refactor.

src/background/extraction.js
@@ -1,0 +1,44 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * Functions to support product extraction.
+ * @module
+ */
+
+import {BROWSER_ACTION_URL} from 'commerce/config';
+import {updateProductWithExtracted} from 'commerce/background/price_updates';
+import {extractedProductShape} from 'commerce/state/products';
+import {validatePropType} from 'commerce/utils';
+
+/**
+ * Triggers background tasks when a product is extracted from a webpage. Along
+ * with normal page navigation, this is also run when the prices are being
+ * updated in the background.
+ *
+ * @param {ExtractedProduct} extracted
+ * @param {MessageSender} sender
+ *  The sender for the content script that extracted this product
+ */
+export function handleExtractedProductData(extractedProduct, sender) {
+  // Do nothing if the extracted product is missing fields.
+  const result = validatePropType(extractedProduct, extractedProductShape);
+  if (result !== undefined) {
+    return;
+  }
+
+  // Update the toolbar icon's URL with the current page's product if we can
+  if (sender.tab) {
+    const url = new URL(BROWSER_ACTION_URL);
+    url.searchParams.set('extractedProduct', JSON.stringify(extractedProduct));
+
+    browser.browserAction.setPopup({
+      popup: url.href,
+      tabId: sender.tab.id,
+    });
+  }
+
+  // Update saved product data if it exists
+  updateProductWithExtracted(extractedProduct);
+}

src/background/index.js
@@ -11,70 +11,14 @@
 
 import {
   BADGE_ALERT_BACKGROUND,
-  BROWSER_ACTION_URL,
   FIRST_RUN_URL,
   PRICE_CHECK_TIMEOUT_INTERVAL,
 } from 'commerce/config';
+import {handleExtractedProductData} from 'commerce/background/extraction';
 import {handleNotificationClicked, handlePriceAlerts} from 'commerce/background/price_alerts';
-import {updateProductWithExtracted, updatePrices} from 'commerce/background/prices';
+import {handleWebRequest, updatePrices} from 'commerce/background/price_updates';
 import store from 'commerce/state';
-import {extractedProductShape} from 'commerce/state/products';
 import {loadStateFromStorage} from 'commerce/state/sync';
-import {validatePropType} from 'commerce/utils';
-
-/**
- * Triggers background tasks when a product is extracted from a webpage. Along
- * with normal page navigation, this is also run when the prices are being
- * updated in the background.
- *
- * @param {ExtractedProduct} extracted
- * @param {MessageSender} sender
- *  The sender for the content script that extracted this product
- */
-function handleExtractedProductData(extractedProduct, sender) {
-  // Do nothing if the extracted product is missing fields.
-  const result = validatePropType(extractedProduct, extractedProductShape);
-  if (result !== undefined) {
-    return;
-  }
-
-  if (sender.tab) {
-    // Update the toolbar popup the next time it is opened with the current page's product
-    const url = new URL(BROWSER_ACTION_URL);
-    url.searchParams.set('extractedProduct', JSON.stringify(extractedProduct));
-
-    browser.browserAction.setPopup({
-      popup: url.href,
-      tabId: sender.tab.id,
-    });
-
-    // Update the toolbar popup while it is open with the current page's product
-    if (sender.tab.active) {
-      browser.runtime.sendMessage({
-        subject: 'extracted-product',
-        extractedProduct,
-      });
-    }
-  }
-
-  // Update saved product data if it exists
-  updateProductWithExtracted(extractedProduct);
-}
-
-/**
- * Remove the x-frame-options header, so that the product page can load in the
- * background page's iframe.
- */
-function handleWebRequest(details) {
-  // only remove the header if this extension's background page made the request
-  if (details.documentUrl === window.location.href) {
-    const responseHeaders = details.responseHeaders.filter(
-      header => !header.name.toLowerCase().includes('x-frame-options'),
-    );
-    return {responseHeaders};
-  }
-  return {responseHeaders: details.responseHeaders};
-}
 
 (async function main() {
   // Set browser action default badge color, which can't be set via manifest

src/background/price_updates.js
@@ -13,6 +13,21 @@ import {addPriceFromExtracted, getLatestPriceForProduct} from 'commerce/state/pr
 import {getAllProducts, getProduct, getProductIdFromExtracted} from 'commerce/state/products';
 
 /**
+ * Remove the x-frame-options header, so that the product page can load in the
+ * background page's iframe.
+ */
+export function handleWebRequest(details) {
+  // only remove the header if this extension's background page made the request
+  if (details.documentUrl === window.location.href) {
+    const responseHeaders = details.responseHeaders.filter(
+      header => !header.name.toLowerCase().includes('x-frame-options'),
+    );
+    return {responseHeaders};
+  }
+  return {responseHeaders: details.responseHeaders};
+}
+
+/**
  * Find products that are due for price updates and update them.
  */
 export function updatePrices() {

commit 9970de245740424ff58410c8205b05ea86181877

Fix #76 regression caused by PR#123
Live updates of the popup were broken in commit 34f860d1554bf63e1fc3 in PR#123 which refactored ./src/background/index.js by breaking it out into different scripts. The block that sent the message with extracted product information from the background script to the browser action script somehow got lost, despite the fact that PR#123 was rebased before merging.

src/background/extraction.js
@@ -28,11 +28,20 @@ export function handleExtractedProductData(extractedProduct, sender) {
     return;
   }
 
-  // Update the toolbar icon's URL with the current page's product if we can
   if (sender.tab) {
     const tabId = sender.tab.id;
+
+    // Update the toolbar icon's URL with the current page's product if we can
     const url = new URL(BROWSER_ACTION_URL);
     url.searchParams.set('extractedProduct', JSON.stringify(extractedProduct));
+
+    // Update the toolbar popup while it is open with the current page's product
+    if (sender.tab.active) {
+      browser.runtime.sendMessage({
+        subject: 'extracted-product',
+        extractedProduct,
+      });
+    }
 
     browser.browserAction.setPopup({popup: url.href, tabId});
     browser.browserAction.setBadgeBackgroundColor({color: BADGE_DETECT_BACKGROUND, tabId});