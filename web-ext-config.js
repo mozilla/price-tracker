c306a32421ab7abefd58d1a73572668e7f095e76
Michael Kelly <mkelly@mozilla.com>
2018-09-24 16:51:04 -0700

    Review fixes.

--- web-ext-config.js
+++ web-ext-config.js
@@ -3,6 +3,7 @@ module.exports = {
     pref: [
       'extensions.shopping@mozilla.org.priceCheckInterval=6000',
       'extensions.shopping@mozilla.org.priceCheckTimeoutInterval=6000',
+      'extensions.shopping@mozilla.org.iframeTimeout=5000',
     ],
     startUrl: [
       'http://www.mkelly.me/fake-product-page/',
