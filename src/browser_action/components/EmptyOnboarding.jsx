3aef8d5715ec13c99e46a9ed0fa8294cd84ecc6c
Michael Kelly <mkelly@mozilla.com>
2018-09-27 17:55:16 -0700

    Fix #105: Update styling on the empty onboarding page.

--- src/browser_action/components/EmptyOnboarding.jsx
+++ src/browser_action/components/EmptyOnboarding.jsx
@@ -6,6 +6,7 @@ import autobind from 'autobind-decorator';
 import React from 'react';
 
 import TrackProductButton from 'commerce/browser_action/components/TrackProductButton';
+import config from 'commerce/config';
 import {extractedProductShape} from 'commerce/state/products';
 
 import 'commerce/browser_action/components/EmptyOnboarding.css';
@@ -24,14 +25,58 @@ export default class EmptyOnboarding extends React.Component {
     extractedProduct: null,
   }
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      learnMoreHref: null,
+    };
+  }
+
+  /**
+   * Fetch the link to the support page on mount, since we can't fetch it async
+   * during the render.
+   */
+  async componentDidMount() {
+    this.setState({
+      learnMoreHref: await config.get('supportUrl'),
+    });
+  }
+
+  /**
+   * Open a new tab and close the popup when links are clicked.
+   */
+  handleClick(event) {
+    if (event.target.href) {
+      event.preventDefault();
+      browser.tabs.create({url: event.target.href});
+      window.close();
+    }
+  }
+
   render() {
     const {extractedProduct} = this.props;
+    const {learnMoreHref} = this.state;
     return (
-      <div className="empty-onboarding">
-        <h1 className="cta">Get notified when the price drops!</h1>
-        <div className="description">
-          Firefox can monitor this product and alert you when the price is right!
-        </div>
+      <div className="empty-onboarding" onClick={this.handleClick}>
+        <img className="hero" src={browser.extension.getURL('img/shopping-welcome.svg')} alt="" />
+        {/*
+          JSX deviates from HTML and removes newlines separating text nodes and components:
+          https://reactjs.org/blog/2014/02/20/react-v0.9.html#jsx-whitespace
+
+          We could futz with the margin on these components, but that would depend on how they're
+          laid out here, which involves coordinating two files. The one-file solution is to add
+          {' '} in the spots where whitespace would otherwise be collapsed.
+        */}
+        <p className="description">
+          Add products you want to buy from
+          {' '}<a href="https://www.amazon.com">Amazon</a>,
+          {' '}<a href="https://www.bestbuy.com/">Best Buy</a>,
+          {' '}<a href="https://www.ebay.com/">eBay</a>,
+          {' '}<a href="https://www.homedepot.com/">Home Depot</a>, and
+          {' '}<a href="https://www.walmart.com/">Walmart</a>
+          {' '}to your Price Watcher list, and Firefox will notify you if the price drops.
+        </p>
+        <a href={learnMoreHref} className="learn-more">Learn More</a>
         <TrackProductButton className="button" extractedProduct={extractedProduct} />
       </div>
     );
