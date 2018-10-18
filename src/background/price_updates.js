707b07848ed2420d6cae800314f66e323cf46381
Bianca Danforth <biancadanforth@gmail.com>
2018-10-18 12:51:12 -0700

    Fix #166: Load background iframes for price updates one at a time
    Previously, in the worst case scenario that a user adds a bunch (15+) of products at close to the same time, our background price checking would load several product pages each in its own iframe in the background page at once. This introduced a lot of jank (average FPS 14 for 15 tracked products added at the same time).
    
    This patch ensures that when more than one product needs a price update check, that the list is queued up, so that only one product page is loaded into an iframe in the background page at a time (average FPS 45 for 15 tracked products added at the same time).
    
    Notes:
    * These FPS values are for a worst case scenario. Most users will not be adding new products to the list within a couple seconds of each other, so most of the time, the number of products in queue for a background price update should be closer to 0 than 15.
    * For reference, a baseline FPS for 0 tracked products is 59 and for 1 tracked product is 54.
    * It is technically possible to load more than one iframe at a time with this patch, however, that is only the case when `(n * iframeTimeout) > priceCheckInterval || (n * iframeTimeout > priceCheckTimeoutInterval). With the current default values for these config timeouts/intervals, the user would need to be tracking at least 90 items for this to occur.
    * Here is the `web-ext-config.js` I used for testing this patch:
    
    ```javascript
    module.exports = {
      run: {
        pref: [
          'extensions.shopping@mozilla.org.priceCheckInterval=45000',
          'extensions.shopping@mozilla.org.priceCheckTimeoutInterval=45000',
          'extensions.shopping@mozilla.org.iframeTimeout=8000',
        ],
        startUrl: [
          'https://www.amazon.com/KitchenAid-KL26M1XER-Professional-6-Qt-Bowl-Lift/dp/B01LYV1U30?smid=ATVPDKIKX0DER&pf_rd_p=0c7b792f-241a-4510-94f4-dd184a76f201&pf_rd_r=AZD7BGV3JZGTB23F30X3',
          'https://www.amazon.com/dp/B077YG22Y9/ref=sspa_dk_detail_3?psc=1&pd_rd_i=B077YG22Y9&pd_rd_wg=mfQm0&pd_rd_r=K27A84Z9Y01R47H7BJWR&pd_rd_w=km01K',
          'https://www.amazon.com/Lindt-Assorted-Chocolate-Gourmet-Truffles/dp/B004XDMS3C/ref=sr_1_6_s_it?s=grocery&ie=UTF8&qid=1533437720&sr=1-6&keywords=chocolates&th=1',
          'https://www.ebay.com/p/Best-Choice-Products-650W-6-speed-5-5QT-Kitchen-Food-Stand-Mixer-with-Stainless-Steels-Bowl-Black/3018375728?iid=253733404998',
          'https://www.ebay.com/itm/Philadelphia-Candies-Milk-Chocolate-Covered-Assorted-Nuts-1-Pound-Gift-Box/263103368447?epid=1301555428&hash=item3d422eccff',
          'https://www.ebay.com/itm/Dell-Inspiron-7570-15-6-Touch-Laptop-i7-8550U-1-8GHz-8GB-1TB-NVIDIA-940MX-W10/263827294291',
          'https://www.walmart.com/ip/KitchenAid-Classic-Series-4-5-Quart-Tilt-Head-Stand-Mixer-Onyx-Black-K45SSOB/29474640',
          'https://www.walmart.com/ip/HP-15-bs212wm-15-6-Laptop-Windows-10-Intel-Celeron-N4000-Processor-4GB-Memory-500GB-Hard-Drive-DVD-Jet-Black/139270579',
          'https://www.walmart.com/ip/Reese-s-Kit-Kat-Chocolate-Candy-Miniatures-Assortment-40-Oz/10449915',
          'https://www.bestbuy.com/site/kitchenaid-artisan-tilt-head-stand-mixer-ocean-drive/6133651.p?skuId=6133651',
          'https://www.bestbuy.com/site/swiss-miss-milk-chocolate-hot-cocoa-k-cup-pods-16-pack/4700838.p?skuId=4700838',
          'https://www.bestbuy.com/site/jbl-everest-elite-750nc-wireless-over-ear-noise-cancelling-headphones-gunmetal/5840136.p?skuId=5840136',
          'https://www.homedepot.com/p/KitchenAid-Classic-4-5-Qt-Tilt-Head-White-Stand-Mixer-K45SSWH/202546032',
          'https://www.homedepot.com/p/Flossugar-1-2-Gal-Chocolate-93216CT/302429246',
          'https://www.homedepot.com/p/Commercial-Electric-Around-the-Neck-Premium-Bluetooth-Stereo-Earbuds-HD0855/300372796',
          'about:debugging',
        ],
      },
    };
    ```

--- src/background/price_updates.js
+++ src/background/price_updates.js
@@ -12,6 +12,7 @@ import store from 'commerce/state';
 import {shouldUpdatePrices} from 'commerce/privacy';
 import {addPriceFromExtracted, getLatestPriceForProduct} from 'commerce/state/prices';
 import {getAllProducts, getProduct, getProductIdFromExtracted} from 'commerce/state/products';
+import {wait} from 'commerce/utils';
 
 /**
  * Remove the x-frame-options header, so that the product page can load in the
@@ -41,10 +42,15 @@ export async function updatePrices() {
   const now = new Date();
   const priceCheckInterval = await config.get('priceCheckInterval');
 
+  const delay = await config.get('iframeTimeout');
   for (const product of products) {
     const priceEntry = getLatestPriceForProduct(state, product.id);
     if (now - priceEntry.date > priceCheckInterval) {
       fetchLatestPrice(product);
+      // Don't load another iframe until the previous one has been removed.
+      await wait(delay); // eslint-disable-line no-await-in-loop
+      // Cleanup previous product's iframe whether it is finished or not.
+      document.getElementById(product.id).remove();
     }
   }
 }
@@ -71,11 +77,6 @@ async function fetchLatestPrice(product) {
   iframe.height = 950;
   iframe.setAttribute('sandbox', 'allow-scripts allow-same-origin allow-forms');
   document.body.appendChild(iframe);
-
-  // Cleanup iframe whether it is finished or not.
-  setTimeout(() => {
-    document.getElementById(product.id).remove();
-  }, await config.get('iframeTimeout'));
 }
 
 /**
