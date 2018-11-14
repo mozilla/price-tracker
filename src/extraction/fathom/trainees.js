418a9cf470027abdb6a057fac012a9eab169ab92
Erik Rose <erik@mozilla.com>
2018-11-14 16:50:12 -0500

    Make the image trainee train only the image-affecting coeffs, for speed.

--- src/extraction/fathom/trainees.js
+++ src/extraction/fathom/trainees.js
@@ -15,14 +15,20 @@ import RulesetFactory from './ruleset_factory';
  * UI, from which you can kick off a training run.
  */
 const trainees = new Map();
+const viewportSize = {width: 1680, height: 950};
 
 const commonRuleset = {
     coeffs: [2, 7, -2, 5, -4, 3, 1, -17, 4, 5, 15, 10],
-    viewportSize: {width: 1680, height: 950},
+    viewportSize,
     rulesetMaker: coeffs => (new RulesetFactory(coeffs)).makeRuleset()
 };
 
-trainees.set('image', commonRuleset);
+trainees.set('image', {
+    coeffs: [5, 9, 3, 4],  // [5, 9, 3, 5] gets 98.7% training
+    viewportSize,
+    // Using Nan as a don't-care value:
+    rulesetMaker: coeffs => (new RulesetFactory([NaN, NaN, NaN, NaN, NaN, NaN, coeffs[0], NaN, NaN, NaN, NaN, coeffs[1], coeffs[2], coeffs[3]])).makeRuleset()
+});
 trainees.set('title', commonRuleset);
 trainees.set('price', commonRuleset);
 
