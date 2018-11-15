34868795054ddc3ac5f1d37b96d271e606b06d8d
Erik Rose <erik@mozilla.com>
2018-11-15 16:51:34 -0500

    Move tuned image coeffs into master vector.

--- src/extraction/fathom/trainees.js
+++ src/extraction/fathom/trainees.js
@@ -18,13 +18,13 @@ const trainees = new Map();
 const viewportSize = {width: 1680, height: 950};
 
 const commonRuleset = {
-    coeffs: [2, 7, -2, 5, -4, 3, 1, -17, 4, 5, 15, 10],
+    coeffs: [2, 7, -2, 5, -4, 3, 5, -17, 4, 5, 15, 9, 3, 4],
     viewportSize,
     rulesetMaker: coeffs => (new RulesetFactory(coeffs)).makeRuleset()
 };
 
 trainees.set('image', {
-    coeffs: [5, 9, 3, 4],  // [5, 9, 3, 5] gets 98.7% training
+    coeffs: [5, 9, 3, 4],
     viewportSize,
     // Using Nan as a don't-care value:
     rulesetMaker: coeffs => (new RulesetFactory([NaN, NaN, NaN, NaN, NaN, NaN, coeffs[0], NaN, NaN, NaN, NaN, coeffs[1], coeffs[2], coeffs[3]])).makeRuleset()
